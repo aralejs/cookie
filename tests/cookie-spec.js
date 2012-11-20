87f5a55377b8548f69f6790720725c3d1a546cfd
lepture <lepture@me.com>
2012-11-20 13:41:47 +0800

    use mocha

--- tests/cookie-spec.js
+++ tests/cookie-spec.js
@@ -14,27 +14,27 @@ define(function(require) {
 
             it('should return the cookie value for the given name.', function() {
 
-                expect(Cookie.get('_sea_test_1')).toBe('1');
-                expect(Cookie.get('_sea_test_2')).toBe('');
-                expect(Cookie.get('_sea_test_3')).toBe('');
-                expect(Cookie.get('_sea_test_4[t]')).toBe('xx');
+                expect(Cookie.get('_sea_test_1')).to.equal('1');
+                expect(Cookie.get('_sea_test_2')).to.equal('');
+                expect(Cookie.get('_sea_test_3')).to.equal('');
+                expect(Cookie.get('_sea_test_4[t]')).to.equal('xx');
 
             });
 
             it('should return undefined for non-existing name.', function() {
 
-                expect(Cookie.get('_sea_test_none')).toBeUndefined();
-                expect(function(){ Cookie.get(true); }).toThrow();
-                expect(function(){ Cookie.get({}); }).toThrow();
-                expect(function(){ Cookie.get(null); }).toThrow();
+                expect(Cookie.get('_sea_test_none')).to.equal(undefined);
+                expect(function(){ Cookie.get(true); }).to.throwError();
+                expect(function(){ Cookie.get({}); }).to.throwError();
+                expect(function(){ Cookie.get(null); }).to.throwError();
 
             });
 
             it('should throw error for invalid name.', function() {
 
-                expect(function(){ Cookie.get(true); }).toThrow();
-                expect(function(){ Cookie.get({}); }).toThrow();
-                expect(function(){ Cookie.get(null); }).toThrow();
+                expect(function(){ Cookie.get(true); }).to.throwError();
+                expect(function(){ Cookie.get({}); }).to.throwError();
+                expect(function(){ Cookie.get(null); }).to.throwError();
 
             });
 
@@ -45,16 +45,16 @@ define(function(require) {
             it('should set a cookie with a given name and value.', function() {
 
                 Cookie.set('_sea_test_11', 'xx');
-                expect(Cookie.get('_sea_test_11')).toBe('xx');
+                expect(Cookie.get('_sea_test_11')).to.equal('xx');
 
                 Cookie.set('_sea_test_12', 'xx', { expires: -1 });
-                expect(Cookie.get('_sea_test_12')).toBeUndefined();
+                expect(Cookie.get('_sea_test_12')).to.equal(undefined);
 
                 Cookie.set('_sea_test_13', '2', {
                     expires: new Date(2099, 1, 1),
                     path: '/'
                 });
-                expect(Cookie.get('_sea_test_13')).toBe('2');
+                expect(Cookie.get('_sea_test_13')).to.equal('2');
 
                 Cookie.remove('_sea_test_14');
                 Cookie.set('_sea_test_14', '4', {
@@ -62,7 +62,7 @@ define(function(require) {
                     path: '/',
                     secure: true
                 });
-                expect(Cookie.get('_sea_test_14')).toBeUndefined();
+                expect(Cookie.get('_sea_test_14')).to.equal(undefined);
 
             });
 
@@ -74,7 +74,7 @@ define(function(require) {
 
                 Cookie.set('_sea_test_21', 'xx');
                 Cookie.remove('_sea_test_21');
-                expect(Cookie.get('_sea_test_21')).toBeUndefined();
+                expect(Cookie.get('_sea_test_21')).to.equal(undefined);
 
                 Cookie.set('_sea_test_22', 'xx', {
                     expires: new Date(2099, 1, 1),
@@ -83,7 +83,7 @@ define(function(require) {
                 Cookie.remove('_sea_test_22', {
                     path: '/'
                 });
-                expect(Cookie.get('_sea_test_22')).toBeUndefined();
+                expect(Cookie.get('_sea_test_22')).to.equal(undefined);
 
             });
         });
